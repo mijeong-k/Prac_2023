437b1fb433cd409419ada58855be40831deceba0
Class01 <Class01@DESKTOP-1KRANK2>
2023-04-26 16:36:51 +0900

    출력_print,println,printf 차이점

--- Practice/src/Prac_2304026.java
+++ Practice/src/Prac_2304026.java
@@ -2,11 +2,11 @@
 public class Prac_2304026 {
 
 	public static void main(String[] args) {
-//		System.out.print("Hello");
-//		System.out.print("World!");
+		System.out.print("Hello");
+		System.out.print("World!\n");
 
-//		System.out.println("Hello");	
-//		System.out.println("World!");
+		System.out.println("Hello");	
+		System.out.println("World!");
 
 		System.out.printf("Hello");
 		System.out.printf("World!");
